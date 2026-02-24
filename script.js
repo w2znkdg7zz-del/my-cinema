a667ac66a45eb2b8e43fd644684d6626c09f2926
w2znkdg7zz-del <roomful-visit-6g@icloud.com>
2026-02-24 22:32:30 +0200

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
    CONFIG
 ================================ */
 
-const TRAKT_ID = 'YOUR_TRAKT_ID';
-const TRAKT_SECRET = 'YOUR_TRAKT_SECRET';
-const TMDB_KEY = 'YOUR_TMDB_KEY';
+const TRAKT_ID = 'caae7a3191de89620d5a2f2955ebce640215e6a81b0cc7657b773de5edebfd40';
+const TRAKT_SECRET = '35a89bd5967a9151de677fd44a4872ab93efba1cc09fee80c27b9176459ece46';
+const TMDB_KEY = '37cc8cb617e62d17e6180754e7a94139';
 const REDIRECT_URI = 'https://w2znkdg7zz-del.github.io/my-cinema/';
 
 /* ================================
